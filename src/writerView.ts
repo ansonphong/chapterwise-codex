5b937eb7ad36963119e7b8efb6f320c844566c68
phong <phong@phong.com>
2025-12-15 14:31:37 -0800

    Update save button icon to cleaner design

--- src/writerView.ts
+++ src/writerView.ts
@@ -1090,10 +1090,8 @@ export class WriterViewManager {
     <div class="header-right">
       <span class="word-count" id="wordCount">${wordCount} words</span>
       <button class="save-btn" id="saveBtn" title="Save (Ctrl+S)">
-        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" style="stroke: currentColor; stroke-width: 1.5; stroke-linecap: round; stroke-linejoin: round;">
-          <rect x="2" y="2" width="12" height="12" rx="1"/>
-          <path d="M2 6h12"/>
-          <rect x="6" y="9" width="4" height="3" rx="0.5"/>
+        <svg width="16" height="16" viewBox="0 0 32 32" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path d="M27.71,9.29l-5-5A1,1,0,0,0,22,4H6A2,2,0,0,0,4,6V26a2,2,0,0,0,2,2H26a2,2,0,0,0,2-2V10A1,1,0,0,0,27.71,9.29ZM12,6h8v4H12Zm8,20H12V18h8Zm2,0V18a2,2,0,0,0-2-2H12a2,2,0,0,0-2,2v8H6V6h4v4a2,2,0,0,0,2,2h8a2,2,0,0,0,2-2V6.41l4,4V26Z"/>
         </svg>
       </button>
     </div>
